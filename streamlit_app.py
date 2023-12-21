e129993fc25df0b14e2dcdf5c7c54d740a0f3a89
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-21 14:17:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,18 @@ col1.image('https://github.com/andrejarenkow/PainelOvitrampas/blob/main/logo_cev
 col2.title('Demo mario')
 col3.image('https://github.com/andrejarenkow/PainelOvitrampas/blob/main/logo_estado%20(3).png?raw=true', width=300)
 
+coluna_filtro, coluna_tabela = st.columns([1,2])
 
 dados = pd.read_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vSdSfuuSRSruaUrc7zpPqWzbPBeTKDGB-Y5xgfIhZND_gityoaYJp-_ja_P5hBBKyFbHA_5y70zPRpt/pub?gid=1357651511&single=true&output=tsv')
 
 municipio = st.selectbox('Selecione o município', sorted(dados['Município da Unidade Sentinela'].unique()))
 
-filtro = dados['Município da Unidade Sentinela']==municipio
+with coluna_filtro:
+    
+    filtro = dados['Município da Unidade Sentinela']==municipio
 
 dados_municipio = dados[filtro]
 
-dados_municipio
+with coluna_tabela:
+
+    dados_municipio
