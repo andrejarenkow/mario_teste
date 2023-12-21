8c1cc14160dd37f507dd1f65184036f5d053f568
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-21 14:18:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,13 @@ coluna_filtro, coluna_tabela = st.columns([1,2])
 
 dados = pd.read_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vSdSfuuSRSruaUrc7zpPqWzbPBeTKDGB-Y5xgfIhZND_gityoaYJp-_ja_P5hBBKyFbHA_5y70zPRpt/pub?gid=1357651511&single=true&output=tsv')
 
-municipio = st.selectbox('Selecione o município', sorted(dados['Município da Unidade Sentinela'].unique()))
+
 
 with coluna_filtro:
-    
-    filtro = dados['Município da Unidade Sentinela']==municipio
+    municipio = st.selectbox('Selecione o município', sorted(dados['Município da Unidade Sentinela'].unique()))
+
+
+filtro = dados['Município da Unidade Sentinela']==municipio
 
 dados_municipio = dados[filtro]
 
