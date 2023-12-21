3b292db83229f8236e8a054c6e0269fea8c5afe1
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-21 14:12:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,8 @@ col1, col2, col3 = st.columns([1,4,1])
 col1.image('https://github.com/andrejarenkow/PainelOvitrampas/blob/main/logo_cevs%20(1).png?raw=true', width=200)
 col2.title('Demo mario')
 col3.image('https://github.com/andrejarenkow/PainelOvitrampas/blob/main/logo_estado%20(3).png?raw=true', width=300)
+
+
+dados = pd.read_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vSdSfuuSRSruaUrc7zpPqWzbPBeTKDGB-Y5xgfIhZND_gityoaYJp-_ja_P5hBBKyFbHA_5y70zPRpt/pub?gid=1357651511&single=true&output=tsv')
+
+dados
